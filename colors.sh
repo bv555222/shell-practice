18b686fcdab240830c8f2ead6c5584ee47b10971
bv555222 <bv555222@gmail.com>
2026-05-25 20:18:55 +0530

    conditions1

--- colors.sh
+++ colors.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-echo -e "\e[32m Hellow World will be in green]"
+
+RED="\e[31m"
+GREEN="\e[32m"
+YELLOW="\e[33m"
+DEFAULT="\e[0m"
+
+echo -e "$RED Hello World will be in red $DEFAULT"
+echo -e "$GREEN Hello World will be in green $DEFAULT"
+echo -e "$YELLOW Hello World will be in yellow $DEFAULT"
