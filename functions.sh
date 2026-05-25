c2a1d05e19628ec678def0fd91f6839a07b6b23b
bv555222 <bv555222@gmail.com>
2026-05-25 20:53:57 +0530

    conditions1

--- functions.sh
+++ functions.sh
@@ -21,22 +21,22 @@ VALIDATE()
 {
     if [ $1 -ne 0 ]
     then
-        echo "$R Installing $2 is ... FAILURE $D"
+        echo $R"Installing $2 is ... FAILURE"$D
         exit 1
     else
-        echo "$G Installing $2 is ... SUCCESS $D"
+        echo $G"Installing $2 is ... SUCCESS"$D 
         exit 0
     fi
 }
 
-echo  "$G Checking Ansible package is already installed or not $D"
+echo  $G"Checking Ansible package is already installed or not"$D
 dnf list installed ansible
 if [ $? -eq 0 ]
 then 
-    echo "$G Ansible is already installed $D"
+    echo $G"Ansible is already installed"$D 
     exit 0
 else
-    echo "$Y Ansible is not installed... going to install it $D"
+    echo $Y"Ansible is not installed... going to install it"$D
     dnf install ansible -y
     VALIDATE $? "Ansible"
 fi
