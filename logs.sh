b741ec62b29b30a00e8c22580bd1ae96c9ce9473
bv555222 <bv555222@gmail.com>
2026-05-25 23:13:07 +0530

    conditions1

--- logs.sh
+++ logs.sh
@@ -7,7 +7,7 @@ Y="\e[33m"
 D="\e[0m"
 
 LOGS_FOLDER="/var/logs/shell_practice"
-SCRIPT_NAME=$( $0 | cut -d "." -f1)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
 
 mkdir -p $LOGS_FOLDER
