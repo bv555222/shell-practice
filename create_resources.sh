f27ef997aa1fcdbce201f7d4589b8200569a0022
bv555222 <bv555222@gmail.com>
2026-06-06 17:15:28 +0530

    create resource

---
+++ create_resources.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+AMI_ID="ami-0220d79f3f480ecf5"
+SG_ID="sg-0ab148400ac1bbfb5"
+INSTANCES=("mysql" "user" "cart" "checkout")
+
+for instances in "${INSTANCES[@]}"; 
+do 
+    echo "Creating instance: $instances"
+    aws ec2 run-instances \
+    --image-id $AMI_ID \
+    --security-group-ids $SG_ID \
+    --instance-type t3.micro \
+    --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instances}]"
+    echo -e "${G} $instances instance is created successfully ${D}"
+    echo -e "${G} \n ${D}"
+done
